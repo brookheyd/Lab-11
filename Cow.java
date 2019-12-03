b54114576fe9709462e17a61630b8b3ceb319d74
brookheyd <brookheyd@gmail.com>
2019-12-03 13:33:51 -0500

    Cow

---
+++ Cow.java
@@ -0,0 +1,23 @@
+public class Cow
+{
+    String name ="";
+    String image="";
+    public Cow(String name)
+    {
+        this.name = name;
+    }
+    public String getName()
+    {
+        return name;//returns the name
+    }
+    public String getImage()
+    {
+        return image;//returns the image
+    }
+    public void setImage(String _image)
+    {
+        this.image = _image;
+    }
+
+
+}
